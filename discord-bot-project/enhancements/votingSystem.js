386524e20300afc4eeb51c99be885cccd3ea4faa
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-13 22:09:31 +0530

    Generated: `discord-bot-project/enhancements/votingSystem.js`

---
+++ discord-bot-project/enhancements/votingSystem.js
@@ -0,0 +1,15 @@
+const { Client, Intents } = require('discord.js');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_VOICE_STATES] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on('messageCreate', async (message) => {
+  if (message.content === '!skip') {
+    // Logic to implement the voting system for users to democratically skip songs
+  }
+});
+
+client.login('your-bot-token');
