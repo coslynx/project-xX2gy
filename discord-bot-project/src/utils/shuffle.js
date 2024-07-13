3c4dfbe08f095a98bb73b6789d4bd73588b2f54e
Drishtant AKA DRIX10 <drixisgod@gmail.com>
2024-07-13 22:09:16 +0530

    Generated: `discord-bot-project/src/utils/shuffle.js`

---
+++ discord-bot-project/src/utils/shuffle.js
@@ -0,0 +1,9 @@
+const shuffle = (queue) => {
+    for (let i = queue.length - 1; i > 0; i--) {
+        const j = Math.floor(Math.random() * (i + 1));
+        [queue[i], queue[j]] = [queue[j], queue[i]];
+    }
+    return queue;
+};
+
+module.exports = shuffle;
